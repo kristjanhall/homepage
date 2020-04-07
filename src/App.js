74746e13574b918c8d18420e72989469dce2dcb8
Kristján Hall <kristjanhall@gmail.com>
2020-04-07 20:09:46 +0000

    Adding gravatar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Gravatar from "react-gravatar";
 import "./App.css";
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <Gravatar email="kristjanhall@gmail.com" size={200} rating="pg" />
           <p>kristjanhall.is</p>
         </header>
       </div>
